ed758086c5959eeefc5d101a0f7e2ec242c7ce6b
nbmblueeye <nbmblueyeye@gmail.com>
2024-04-26 12:03:57 +0700

    fifth commit

--- server/server.js
+++ server/server.js
@@ -13,9 +13,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 openAIDbConn();
   
-// host static files
-app.use(express.static("client"));
-
 app.use(cors());
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ extended: false }));
